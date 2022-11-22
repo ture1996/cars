7ee8d350bb8f106a974cd940da58eec923c52d98
ture1996 <turanjanin1996@gmail.com>
2022-11-22 15:15:28 +0100

    First commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -21,6 +21,6 @@
         </style>
     </head>
     <body class="antialiased">
-        <h1 align = 'center'>Welcome to cars project</h1>
+        <h1 align = 'center'>About us</h1>
     </body>
 </html>
